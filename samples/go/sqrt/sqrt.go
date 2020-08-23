02a6692491088860a78f5baa961fce543cbbabc2
tpyle <thomasp162@gmail.com>
2020-08-23 19:46:35 +0000

    Adjusts sqrt.go to use float64

--- samples/go/sqrt/sqrt.go
+++ samples/go/sqrt/sqrt.go
@@ -7,12 +7,11 @@ import (
 )
 
 
-func findSqrt(number float32) (float32) {
-    var x float32 = float32(number)
-    var y float32 = 1.0
-    const e float32 = 0.0000001
+func findSqrt(number float64) (float64) {
+    var x float64 = float64(number)
+    var y float64= 1.0
+    const e float64 = 0.0000001
     for x - y > e {
-        fmt.Println(x,y)
         x = (x + y) / 2.0
         y = number / x
     }
@@ -31,5 +30,5 @@ func main() {
         fmt.Println("Error: Expected to receive a number, recevied", os.Args[1])
         os.Exit(2)
     }
-    fmt.Println(findSqrt(float32(number)))
+    fmt.Println(findSqrt(float64(number)))
 }
