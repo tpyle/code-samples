3c40c370cef866a18d34ec636b534ebaeda0c443
tpyle <thomasp162@gmail.com>
2020-09-14 22:53:50 +0000

    Converts go's prime to use the new method

--- samples/go/04primality/prime.go
+++ samples/go/04primality/prime.go
@@ -9,25 +9,34 @@ import (
 
 
 func isPrime(number float64) (bool) {
-    var limit float64 = math.Sqrt(number)
+    var limit float64 = math.Sqrt(number);
     for i := 2.0; i <= limit; i++ {
         if math.Mod(number, i) == 0 {
-            return false
+            return false;
         }
     }
-    return true
+    return true;
 }
 
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Error: Usage ./prime <number>")
-        os.Exit(1)
+    if len(os.Args) < 2 {
+        fmt.Println("Error: Usage ./prime <number> [<number> ...]");
+        os.Exit(1);
     }
-    number, err := strconv.ParseFloat(os.Args[1], 64)
-    if err != nil {
-        fmt.Println("Error: Expected to receive a number, recevied", os.Args[1])
-        os.Exit(2)
+
+    for _, arg := range os.Args[1:] {
+        number, err := strconv.ParseFloat(arg, 64)
+        if err != nil {
+            fmt.Printf("Error: Expected to receive a number, recevied '%s'\n", arg);
+            os.Exit(2);
+        }
+
+        var primestring = "false";
+        if isPrime(number) {
+            primestring = "true";
+        }
+
+        fmt.Printf("%f => %s\n", number, primestring);
     }
-    fmt.Println(isPrime(number))
 }
