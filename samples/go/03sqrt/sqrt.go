09ce09f0c47997a4240e16730a386c2196d62333
tpyle <thomasp162@gmail.com>
2020-09-14 21:59:53 +0000

    Updates go sqrt to use new format

--- samples/go/03sqrt/sqrt.go
+++ samples/go/03sqrt/sqrt.go
@@ -8,27 +8,30 @@ import (
 
 
 func findSqrt(number float64) (float64) {
-    var x float64 = float64(number)
-    var y float64= 1.0
-    const e float64 = 0.0000001
+    var x float64 = float64(number);
+    var y float64= 1.0;
+    const e float64 = 0.0000001;
     for x - y > e {
-        x = (x + y) / 2.0
-        y = number / x
+        x = (x + y) / 2.0;
+        y = number / x;
     }
-    return x
+    return x;
 }
 
 
 func main() {
-    if (len(os.Args) != 2) {
-        fmt.Println("Error: Usage ./sqrt <number>")
-        os.Exit(1)
+    if (len(os.Args) < 2) {
+        fmt.Println("Error: Usage ./sqrt <number> [<number> ...]");
+        os.Exit(1);
     }
-    var number int = 0
-    number, err := strconv.Atoi(os.Args[1])
-    if err != nil {
-        fmt.Println("Error: Expected to receive a number, recevied", os.Args[1])
-        os.Exit(2)
+
+    for _, arg := range os.Args[1:] {
+        var number int = 0;
+        number, err := strconv.Atoi(arg);
+        if err != nil {
+            fmt.Printf("Error: Expected to receive an integer, received %s\n", arg);
+            os.Exit(2);
+        }
+        fmt.Printf("%d => %f\n", number, findSqrt(float64(number)))
     }
-    fmt.Println(findSqrt(float64(number)))
 }
