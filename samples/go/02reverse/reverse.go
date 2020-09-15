68ec6d4d10bdab8d9b7b46f37d3a5c5958e6ac46
tpyle <thomasp162@gmail.com>
2020-09-15 16:37:45 +0000

    Implements reverse in go

---
+++ samples/go/02reverse/reverse.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Printf("Error: Usage %s <arg> [<arg> ...]\n", os.Args[0]);
+    }
+
+    for i := len(os.Args)-1; i > 0; i-- {
+        fmt.Print(os.Args[i]);
+        if i > 1 {
+            fmt.Print(" ");
+        }
+    }
+    fmt.Println();
+}
