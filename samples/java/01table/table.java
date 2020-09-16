39ddf7ecb3abf2f068061e303ffd6fb1a2bc9a42
tpyle <thomasp162@gmail.com>
2020-09-16 23:43:15 +0000

    Implements table in java

---
+++ samples/java/01table/table.java
@@ -0,0 +1,21 @@
+//package table;
+
+
+public class table {
+    public static final int LIMIT = 5;
+    public static void main(String[] args) {
+        System.out.print("   ");
+        for (int i = 1; i <= LIMIT; i++) {
+            System.out.print(String.format("%-3d", i));
+        }
+        System.out.println();
+        
+        for (int a = 1; a <= LIMIT; a++) {
+            System.out.print(String.format("%-3d", a));
+            for (int b = 1; b <= LIMIT; b++) {
+                System.out.print(String.format("%-3d", a*b));
+            }
+            System.out.println();
+        }
+    }
+}
