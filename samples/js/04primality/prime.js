6a819de274792f72f1a5f4ead67ed4cfc44ca7eb
tpyle <thomasp162@gmail.com>
2020-09-18 01:47:10 +0000

    Modifies js prime to new design

--- samples/js/04primality/prime.js
+++ samples/js/04primality/prime.js
@@ -11,15 +11,17 @@ function isPrime(number) {
 
 
 if (require.main === module) {
-    if (process.argv.length !== 3) {
-        console.error("Error: Usage node prime.js <number>");
+    if (process.argv.length < 3) {
+        console.error("Error: Usage node prime.js <number> [<number> ...]");
         process.exit(1);
     }
-    const [number] = process.argv.slice(2);
+    process.argv.slice(2).map(arg=>{
+    const number = arg;
     const _number = Number(number);
     if (isNaN(_number)) {
-        console.error("Error: <number> must be a numeric value. Received", number);
+        console.error(`Error: <number> must be a numeric value. Received '${number}'`);
         process.exit(2);
     }
-    console.log(isPrime(_number));
+    console.log(`${number} => ${isPrime(_number)}`);
+    });
 }
