a7111bacb6181bd86f1e6b403f26b895062429d8
tpyle <thomasp162@gmail.com>
2020-09-25 00:23:04 +0000

    Implements table in php

---
+++ samples/php/01table/table.php
@@ -0,0 +1,22 @@
+<?php
+
+define("LIMIT", 5);
+
+
+for ($a = 0; $a <= LIMIT; $a++) {
+    if ($a == 0) {
+        printf("%-3s", "");
+    } else {
+        printf("%-3d", $a);
+    }
+    for ($b = 0; $b <= LIMIT; $b++) {
+        if ($a == 0) {
+            printf("%-3d", $b);
+        } else {
+            printf("%-3d", $a * $b);
+        }
+    }
+    printf("\n");
+}
+
+?>
