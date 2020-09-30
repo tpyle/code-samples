644bbc6bfc0ade49ab1bbd16a4b9f36e5f93d062
tpyle <thomasp162@gmail.com>
2020-09-30 03:11:26 +0000

    Modifies python sqrt to use the new method

--- samples/python/03sqrt/sqrt.py
+++ samples/python/03sqrt/sqrt.py
@@ -12,12 +12,14 @@ def find_sqrt(number):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Error: Usage python sqrt.py <number>")
+    if len(sys.argv) < 2:
+        print("Error: Usage python sqrt.py <number> [<number> ...]")
         sys.exit(1)
-    try:
-        number = float(sys.argv[1])
-    except ValueError:
-        print("Error: Expected a number. Received", sys.argv[1])
-        sys.exit(2)
-    print(find_sqrt(number))
+    
+    for arg in sys.argv[1:]:
+        try:
+            number = float(arg)
+        except ValueError:
+            print("Error: Expected a number. Received", arg)
+            sys.exit(2)
+        print("%f => %f" % (number, find_sqrt(number)))
