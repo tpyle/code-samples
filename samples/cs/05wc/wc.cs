8cd8f6dbc4019d5bf775cc38b058d5e47b19147d
tpyle <thomasp162@gmail.com>
2020-10-15 02:21:36 +0000

    Implements wc in C#

---
+++ samples/cs/05wc/wc.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+
+namespace LineCount {
+    class Exec {
+        static void Main(string[] args) {
+            if (args.Length > 1) {
+                Console.Error.WriteLine("Error: Expected to receive at most 1 argument ./wc [<number>]");
+                System.Environment.Exit(1);
+            }
+
+            int lines = 0;
+            if (args.Length == 1) {
+                Console.SetIn(new StreamReader(args[0]));
+            }
+
+            while (Console.ReadLine() != null) {
+                lines++;
+            }
+
+            Console.WriteLine(lines);
+        }
+    }
+}
