10cb454ce5951e58a1a4c26783df32db9c92903b
tpyle <thomasp162@gmail.com>
2020-10-18 23:57:30 +0000

    Implements sqrt in erlang

---
+++ samples/erlang/03sqrt/sqrt.erl
@@ -0,0 +1,41 @@
+#!/usr/bin/env escript
+%% -*- erlang -*-
+%%! -smp enable -sname factorial -mnesia debug verbose
+%%
+
+-define(ACCURACY, 0.000001).
+
+find_sqrt(Number) when Number < 0 ->
+    io:format("Error: Expected a positive integer. Received '~w'~n", [Number]),
+    halt(3);
+find_sqrt(Number) ->
+    find_sqrt(Number, Number, 1.0).
+
+find_sqrt(Number,X,Y) when abs(X-Y) > ?ACCURACY ->
+    X_New = (X+Y)/2,
+    find_sqrt(Number, X_New, Number/X_New);
+find_sqrt(_Number,X,_Y) ->
+    X.
+
+for_args([]) ->
+    [];
+for_args([H|T]) ->
+    try 
+        V = list_to_float(H),
+        io:format("~w => ~w~n", [V, find_sqrt(V)]),
+        for_args(T)
+    catch
+        error:badarg ->
+            bad_arg(H)
+    end.
+
+
+bad_arg(BAD_ARG) ->
+    io:format(standard_error, "Error: Expected a number. Received '~s'~n", [BAD_ARG]),
+    halt(2).
+
+main([]) ->
+    io:format(standard_error, "Error: Usage sqrt <number> [<number> ...]~n", []),
+    halt(1);
+main(ARGS) ->
+    for_args(ARGS).
