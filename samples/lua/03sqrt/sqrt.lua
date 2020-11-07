82265dc3efdd35427fa8e18ea75d7aea365ccfec
tpyle <thomasp162@gmail.com>
2020-11-07 21:04:12 +0000

    Implements sqrt in lua

---
+++ samples/lua/03sqrt/sqrt.lua
@@ -0,0 +1,27 @@
+function printf(s, ...)
+    return io.write(s:format(...))
+end
+
+function eprintf(s, ...)
+    return io.stderr:write(s:format(...))
+end
+
+function find_sqrt (number)
+    local x = number + .0
+    local y = 1.0
+    local e = 0.00000001
+    while (x - y > e) do
+        x = (x+y)/2
+        y = number/x
+    end
+    return x
+end
+
+for i=1,#arg do
+    number = tonumber(arg[i])
+    if number == nil then
+        eprintf("Error: Expected to receive a number. Received '%s'\n", arg[i])
+        os.exit(1)
+    end
+    printf("%s => %f\n", arg[i], find_sqrt(number))
+end
