f10b732206c7db08e531a8b72807ea3cbaeb3571
tpyle <thomasp162@gmail.com>
2020-11-23 02:50:12 +0000

    Cleans up perl sqrt to use constants, strict, and warnings

--- samples/perl/03sqrt/sqrt.pl
+++ samples/perl/03sqrt/sqrt.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+
+use constant ACCURACY => 0.000001;
 
 sub FindSqrt {
     my ($number) = @_;
-    $x = $number;
-    $y = 1;
-    $e = 0.000001;
-    while ($x-$y > $e) {
+    my $x = $number;
+    my $y = 1;
+    while ($x-$y > ACCURACY) {
         $x = ($x + $y) / 2;
         $y = $number / $x;
     }
@@ -14,7 +17,7 @@ sub FindSqrt {
 }
 
 
-$len = scalar(@ARGV);
+my $len = scalar(@ARGV);
 if ($len == 0) {
     print STDERR "Error: Expected to receive $0 <number> [<number> ...]\n";
     exit(1);
@@ -24,6 +27,6 @@ if ($len == 0) {
 foreach my $temp (@ARGV) {
     my $number = int($temp) or die "Expected to receive a number. Received '$temp'\n";
 
-    $val = FindSqrt($number);
+    my $val = FindSqrt($number);
     print("$number => $val\n");
 }
