49816b7e381a19f2a0ac182743e22e0ca80d6d0b
tpyle <thomasp162@gmail.com>
2021-01-13 16:47:24 +0000

    Implements cat in kotlin

---
+++ samples/kotlin/06cat/cat.kt
@@ -0,0 +1,17 @@
+import java.io.File;
+
+fun main(args: Array<String>) {
+    if (args.size > 1) {
+        System.err.println("Error: Usage `kotlin wc [<file>]`");
+        System.exit(1);
+    }
+    if (args.size == 1) {
+        File(args[0]).forEachLine { line -> println(line) };
+    } else {
+        var line: String? = readLine();
+        while (line != null) {
+            println(line);
+            line = readLine();
+        }
+    }
+}
