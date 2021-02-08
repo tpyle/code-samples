a9ad18c10f994608044c08634099d264d2355378
tpyle <thomasp162@gmail.com>
2021-02-08 02:27:23 +0000

    Implements rectangle in shell

---
+++ samples/bash/08objects/rectangle.sh
@@ -0,0 +1,28 @@
+ACTION=$1
+ID=$2
+
+
+case $ACTION in
+    STORE)
+        ID=$RANDOM
+        SIDE_A=$2
+        SIDE_B=$3
+        cat > /tmp/$ID.rectangle <<EOF
+SIDE_A=$SIDE_A
+SIDE_B=$SIDE_B
+EOF
+        echo $ID
+        ;;
+    AREA)
+        source /tmp/$ID.rectangle
+        bc <<EOF
+$SIDE_A * $SIDE_B
+EOF
+        ;;
+    PERIMETER)
+        source /tmp/$ID.rectangle
+        bc <<EOF
+$SIDE_A * 2 + $SIDE_B * 2
+EOF
+        ;;
+esac
