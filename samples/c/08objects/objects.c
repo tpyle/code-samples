5763c207b91ee6764d88a379ccd8f6a0cf0a6564
tpyle <thomasp162@gmail.com>
2021-02-17 01:26:43 +0000

    Removes extra print statements

--- samples/c/08objects/objects.c
+++ samples/c/08objects/objects.c
@@ -11,12 +11,7 @@ int main() {
     printf("P: %f, A: %f\n", squarePerimeter(s), squareArea(s));
     Triangle* t = createTriangle(5, 2);
     printf("P: %f, A: %f\n", trianglePerimeter(t), triangleArea(t));
-    printf("%p %p %p\n", r, s, t);
     deleteRectangle(&r);
-    printf("r\n");
     deleteSquare(&s);
-    printf("s\n");
     deleteTriangle(&t);
-    printf("t\n");
-    printf("%p %p %p\n", r, s, t);
 }
