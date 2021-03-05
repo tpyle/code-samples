4d93b6149487213a50041953a3ac50cd6ccc30b9
tpyle <thomasp162@gmail.com>
2021-03-05 13:30:16 +0000

    Implements trapezoid in dart

--- samples/dart/08objects/objects.dart
+++ samples/dart/08objects/objects.dart
@@ -74,13 +74,32 @@ class Circle extends Shape {
     }
 }
 
+class Trapezoid extends Rectangle {
+    Triangle trianglePart;
+
+    Trapezoid(double height, double totalWidth, double triangleWidth) : super(height, totalWidth) {
+        this.trianglePart = Triangle(height, triangleWidth);
+    }
+
+    double area() {
+        return super.area() - this.trianglePart.area();
+    }
+
+    double perimeter() {
+        return super.perimeter() - this.getHeight() + this.trianglePart.getHypotenuse()
+            - this.trianglePart.getWidth();
+    }
+}
+
 void main() {
     Shape r = Rectangle(5, 2);
-    print("[Rectange] A: ${r.area()} P: ${r.perimeter()}");
+    print("[Rectange]  A: ${r.area()} P: ${r.perimeter()}");
     Shape s = Square(4);
-    print("[Square]   A: ${s.area()} P: ${s.perimeter()}");
+    print("[Square]    A: ${s.area()} P: ${s.perimeter()}");
     Triangle t = Triangle(4, 2);
-    print("[Triangle] A: ${t.area()} P: ${t.perimeter()}");
+    print("[Triangle]  A: ${t.area()} P: ${t.perimeter()}");
     Circle c = Circle(3);
-    print("[Circle]   A: ${c.area()} P: ${c.perimeter()}");
+    print("[Circle]    A: ${c.area()} P: ${c.perimeter()}");
+    Trapezoid p = Trapezoid(5, 7, 4);
+    print("[Trapezoid] A: ${p.area()} P: ${p.perimeter()}");
 }
