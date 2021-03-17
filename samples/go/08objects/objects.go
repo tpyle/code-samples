f509f0163560ce169888afb41cb961fb1566b7ca
tpyle <thomasp162@gmail.com>
2021-03-17 21:52:33 +0000

    Implements Rectangles in go

---
+++ samples/go/08objects/objects.go
@@ -0,0 +1,8 @@
+package main
+
+import "main/shapes"
+
+func main() {
+	r := rectangle.New(5, 2);
+	rectangle.Print(r);
+}
---
+++ samples/go/08objects/shapes/rectangle.go
@@ -0,0 +1,31 @@
+package rectangle
+
+import (
+	"fmt"
+)
+
+type Rectangle struct {
+	width float64
+	height float64
+}
+
+func New(width float64, height float64) (Rectangle) {
+	return Rectangle {
+		width: width,
+		height: height,
+	};
+}
+
+func Area(r Rectangle) float64 {
+	return r.width * r.height;
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * r.width + 2 * r.height;
+}
+
+func Print(r Rectangle) {
+	fmt.Printf("[Rectangle] Area: %f Perimeter %f\n",
+		Area(r),
+		Perimeter(r));
+}
