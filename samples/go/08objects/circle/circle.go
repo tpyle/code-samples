9ec8bcf75fa95535cf1b00c297d269f5da19dce8
tpyle <thomasp162@gmail.com>
2021-03-18 17:28:45 +0000

    Implements circle in go

---
+++ samples/go/08objects/circle/circle.go
@@ -0,0 +1,30 @@
+package circle
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	radius float64
+};
+
+func New(radius float64) Circle {
+	return Circle {
+		radius: radius,
+	}
+}
+
+func Area(c Circle) float64 {
+	return math.Pi * c.radius * c.radius;
+}
+
+func Perimeter(c Circle) float64 {
+	return 2 * math.Pi * c.radius;
+}
+
+func Print(c Circle) {
+	fmt.Printf("[Circle] Area: %f Perimeter: %f\n",
+		Area(c),
+		Perimeter(c));
+}
--- samples/go/08objects/objects.go
+++ samples/go/08objects/objects.go
@@ -2,10 +2,13 @@ package main
 
 import "main/rectangle"
 import "main/square"
+import "main/circle"
 
 func main() {
 	r := rectangle.New(5, 2);
 	rectangle.Print(r);
 	s := square.New(5);
 	square.Print(s);
+	c := circle.New(3);
+	circle.Print(c);
 }
