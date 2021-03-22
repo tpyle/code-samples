97b76acd922f3b3d0fc53710f177433b047876fc
tpyle <thomasp162@gmail.com>
2021-03-22 02:23:59 +0000

    Cleans up printing

--- samples/java/08objects/main/Shape.java
+++ samples/java/08objects/main/Shape.java
@@ -3,4 +3,13 @@ package main;
 public abstract class Shape {
     public abstract double area();
     public abstract double perimeter();
+
+    public String toString() {
+        return String.format(
+            "[%s]\n  Area: %f\n  Perimeter: %f",
+            this.getClass().getSimpleName(),
+            this.area(),
+            this.perimeter()
+        );
+    }
 }
--- samples/java/08objects/main/objects.java
+++ samples/java/08objects/main/objects.java
@@ -6,8 +6,8 @@ import main.Circle;
 public class objects {
     public static void main(String[] args) {
         Rectangle r = new Rectangle(5, 2);
-        System.out.printf("[Rectangle] Area: %f Perimeter: %f\n", r.area(), r.perimeter());
+        System.out.println(r);
         Circle c = new Circle(4);
-        System.out.printf("[Circle]    Area: %f Perimeter: %f\n", c.area(), c.perimeter());
+        System.out.println(c);
     }
 }
