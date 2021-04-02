609a66b68e774e1f8db9e3d919ed632aa01d90ce
tpyle <thomasp162@gmail.com>
2021-04-02 00:49:42 +0000

    Implements Triangle in kotlin

--- samples/kotlin/08objects/objects.kt
+++ samples/kotlin/08objects/objects.kt
@@ -1,10 +1,13 @@
 import shapes.Rectangle;
 import shapes.Square;
+import shapes.Triangle;
 
 fun main() {
     val rectangle = Rectangle(5.0, 2.0);
     val square = Square(5.0);
+    val triangle = Triangle(4.0, 2.0);
 
     println(rectangle);
     println(square);
+    println(triangle);
 }
--- samples/kotlin/08objects/shapes.kt
+++ samples/kotlin/08objects/shapes.kt
@@ -1,5 +1,7 @@
 package shapes;
 
+import kotlin.math.sqrt
+
 fun importTest() {
     println("We cruising");
 }
@@ -16,13 +18,25 @@ open class Rectangle(height: Double, width: Double) : Shape() {
     val height = height;
     val width = width;
 
-    override fun area(): Double {
+    override open fun area(): Double {
         return this.height * this.width;
     }
 
-    override fun perimeter(): Double {
+    override open fun perimeter(): Double {
         return this.height * 2 + this.width * 2;
     }
 }
 
 class Square(side: Double) : Rectangle(side, side);
+
+class Triangle(height: Double, width: Double): Rectangle(height, width) {
+    val hypotenuse = sqrt(this.height * this.height + this.width * this.width);
+
+    override fun area(): Double {
+        return super.area() / 2;
+    }
+
+    override fun perimeter(): Double {
+        return super.perimeter() / 2 + this.hypotenuse;
+    }
+}
