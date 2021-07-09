8215639b5d92a5c3542eb6f12cd9032c83a6ba93
tpyle <thomasp162@gmail.com>
2021-07-09 02:32:20 +0000

    Implements efficient sqrt in python

---
+++ samples/python/09esqrt/esqrt.py
@@ -0,0 +1,32 @@
+import sys
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
+def find_sqrt(number):
+    number = float(number)
+    x = number
+    y = 1.0
+    e = 0.000000001
+    while x - y > e:
+        x = (x + y) / 2.0
+        y = number / x
+    return x
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error: Usage python sqrt.py <number> [<number> ...]")
+        sys.exit(1)
+    
+    with ThreadPoolExecutor() as executor:
+        results = []
+        for arg in sys.argv[1:]:
+            try:
+                number = float(arg)
+            except ValueError:
+                print("Error: Expected a number. Received", arg, file=sys.stderr)
+                continue
+            results.append((arg, executor.submit(find_sqrt, number)))
+        
+        for arg, result in results:
+            print(f"{arg} => {result.result()}")
